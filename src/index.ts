4dc59246fbe69120f9b2f774499a9dc3e6465c20
notforu <aleksndr.muratov@gmail.com>
2022-08-31 15:20:59 +0400

    add albums support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Markup, Scenes, session, Telegraf } from 'telegraf';
+import { createClient } from 'redis';
 import { ADMIN_ID, BOT_TOKEN, CHANNEL_ID } from './constants';
 import { replyWithError } from './utils';
 import { BotContext } from './types';
 
+const mediaGroup = require('telegraf-media-group');
+
 require('dotenv').config();
+
+const redisClient = createClient();
 
 const bot = new Telegraf<BotContext>(BOT_TOKEN);
 
 const startScene = new Scenes.BaseScene<BotContext>('startScene');
+const anonScene = new Scenes.BaseScene<BotContext>('anonScene');
+const stage = new Scenes.Stage([startScene, anonScene] as any);
+bot.use(session());
+bot.use(mediaGroup());
+bot.use(stage.middleware() as any);
+
 startScene.enter(async (ctx) => {
 	try {
 		return ctx.reply(`Хай, че ел(а) сёдня?
@@ -21,7 +32,7 @@ async function onMessage(ctx: BotContext) {
 	try {
 		const message: any = ctx.message;
 		if (message.text === '/start') return (ctx as any).scene.enter('startScene');
-		console.log(message);
+		//console.log(2, message);
 		if (message.photo) {
 			ctx.session = {text: message.caption || '', fileId: message.photo[0].file_id};
 		} else {
@@ -33,9 +44,29 @@ async function onMessage(ctx: BotContext) {
 	}
 }
 
+async function sendMediaGroup(ctx: BotContext, chatId: number, text: string, fileIds: string[]): Promise<string | undefined> {
+	const res = await ctx.telegram.sendMediaGroup(chatId, fileIds.map((fileId, index) => ({
+		type: 'photo',
+		media: fileId,
+		caption: index === 0 ? (text || '') : '',
+	})));
+	return res[0].media_group_id;
+}
+
+bot.on('media_group' as any, async ctx => {
+	try {
+		ctx.session = {
+			text: (ctx as any).mediaGroup[0].caption,
+			fileIds: (ctx as any).mediaGroup.map((message: any) => message.photo[0].file_id),
+		};
+		return (ctx as any).scene.enter('anonScene');
+	} catch (e) {
+		return replyWithError(ctx, e);
+	}
+});
+
 startScene.on('message', onMessage);
 
-const anonScene = new Scenes.BaseScene<BotContext>('anonScene');
 anonScene.enter(async (ctx) => {
 	try {
 		return ctx.reply(`Опубликовать анонимно?`, {
@@ -62,33 +93,44 @@ anonScene.on('callback_query', async (ctx) => {
 		if (!ctx.chat || !ctx.session) return;
 		if (!ctx.callbackQuery.data) throw Error('Incorrect answer');
 		const isAnonymous = Boolean(Number(ctx.callbackQuery.data));
-		let text = (isAnonymous || !ctx.from?.username) ? ctx.session.text : `${ctx.session.text}
+		let text = (isAnonymous || !ctx.from?.username) ? (ctx.session.text || '') : `${ctx.session.text}
 
 @${ctx.from?.username}`;
 		text += `
 
 #че_ел_подписчик`;
-		let res;
-		if (ctx.session.fileId) {
-			res = await ctx.telegram.sendPhoto(
+
+		let postAction: string;
+		if (ctx.session.fileIds) {
+			const fileIds = ctx.session.fileIds;
+			const mediaGroupId = await sendMediaGroup(ctx, ADMIN_ID, text, fileIds);
+			if (!mediaGroupId) throw new Error('No media group ID for album!');
+			await redisClient.set(mediaGroupId, JSON.stringify({ fileIds, text }));
+			postAction = JSON.stringify({
+				type: 'post',
+				mediaGroupId,
+			});
+		} else if (ctx.session.fileId) {
+			const res = await ctx.telegram.sendPhoto(
 				ADMIN_ID,
 				ctx.session.fileId,
 				{caption: text},
 			);
+			postAction = JSON.stringify({
+				type: 'post',
+				messageId: res.message_id,
+			});
 		} else {
-			res = await ctx.telegram.sendMessage(
+			const res = await ctx.telegram.sendMessage(
 				ADMIN_ID,
 				text,
 			);
+			postAction = JSON.stringify({
+				type: 'post',
+				messageId: res.message_id,
+			});
 		}
-		const postAction = JSON.stringify({
-			type: 'post',
-			messageId: res.message_id,
-		});
-		const deleteAction = JSON.stringify({
-			type: 'delete',
-			messageId: res.message_id,
-		});
+
 		await ctx.editMessageText('Ваш пост отправлен на модерацию', {
 			reply_markup: {
 				inline_keyboard: [
@@ -98,12 +140,13 @@ anonScene.on('callback_query', async (ctx) => {
 				],
 			},
 		});
+
 		return ctx.telegram.sendMessage(ADMIN_ID, 'Постим такое?', {
 			reply_markup: {
 				inline_keyboard: [
 					[
 						Markup.button.callback('Да', postAction),
-						Markup.button.callback('Нет', deleteAction),
+						Markup.button.callback('Нет', JSON.stringify({type: 'delete'})),
 					],
 				],
 			},
@@ -113,10 +156,6 @@ anonScene.on('callback_query', async (ctx) => {
 	}
 });
 
-const stage = new Scenes.Stage([startScene, anonScene] as any);
-bot.use(session());
-bot.use(stage.middleware() as any);
-
 bot.command('/start', (ctx) => (ctx as any).scene.enter('startScene'));
 bot.on('message', onMessage);
 
@@ -125,15 +164,23 @@ bot.on('callback_query', async (ctx) => {
 		if (ADMIN_ID !== ctx.from?.id) throw Error('Permission denied');
 		if (!ctx.callbackQuery.data) throw Error('Incorrect answer');
 		const action = JSON.parse(ctx.callbackQuery.data);
-		const {type, messageId} = action;
+		const {type, messageId, mediaGroupId} = action;
 		const emptyExtra = {reply_markup: {inline_keyboard: []}};
 		switch (type) {
 			case 'post':
-				await ctx.telegram.copyMessage(
-					CHANNEL_ID,
-					ADMIN_ID,
-					messageId,
-				);
+				if (mediaGroupId) {
+					const payload = await redisClient.get(mediaGroupId);
+					if (!payload) throw new Error('No payload in cache for mediaGroupId: ' + mediaGroupId);
+					const {fileIds, text} = JSON.parse(payload) as { fileIds: string[]; text: string };
+					await sendMediaGroup(ctx, CHANNEL_ID, text, fileIds);
+					await redisClient.del(mediaGroupId);
+				} else {
+					await ctx.telegram.copyMessage(
+						CHANNEL_ID,
+						ADMIN_ID,
+						messageId,
+					);
+				}
 				await ctx.editMessageText('Сообщение опубликовано', emptyExtra);
 				break;
 			case 'delete':
@@ -149,16 +196,21 @@ bot.on('callback_query', async (ctx) => {
 	}
 });
 
-bot.launch().then(() => console.log('bot started')).catch((e) => console.error('Launch error: ' + e));
+Promise.all([bot.launch(), redisClient.connect()])
+	.then(() => console.log('bot started'))
+	.catch((e) => console.error('Launch error: ' + e));
 
 bot.catch((e) => console.error('Common error: ' + e));
+redisClient.on('error', (err) => console.log('Redis error:', err));
 
 // Enable graceful stop
 process.once('SIGTERM', () => {
 	bot.stop('SIGTERM');
+	redisClient.disconnect();
 	process.exit();
 });
 process.once('SIGINT', () => {
 	bot.stop('SIGINT');
+	redisClient.disconnect();
 	process.exit();
 });
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import { Context } from 'telegraf';
 export interface SessionData {
 	text: string;
 	fileId?: string;
+	fileIds?: string[];
 }
 
 export interface BotContext extends Context {
